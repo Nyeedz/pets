c6198ef4db15c4fb7e2404cd5925c2c522778de2
Luiz Eduardo C <eduselcip@gmail.com>
2019-10-27 03:59:55 -0300

    layout login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.page.html",
-  styleUrls: ["./login.page.scss"]
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
+  loading = false;
   constructor() {}
 
   ngOnInit() {}
+
+  setLoading(state: boolean) {
+    this.loading = state;
+  }
 }
